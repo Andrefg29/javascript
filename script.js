19eeb237d2c478287a2f300511a816bc52f91c3d
Andrefg29 <andre_f_giacomelli@estudante.sesisenai.org.br>
2023-05-04 11:41:24 -0300

    joia

---
+++ script.js
@@ -0,0 +1,22 @@
+function Confirmar(){
+    texto = nome.value
+    teste.innerHTML = texto
+}
+
+function MostrarJanela(){
+    janelinha.style.display = "block";
+}
+
+function Fechar(){
+    janelinha.style.display = "none"
+}
+
+soma = 0
+function Somar(){
+    soma = soma + 1
+    resultado.value = soma + 1
+}
+
+function Imprimir(){
+    window.print();
+}
